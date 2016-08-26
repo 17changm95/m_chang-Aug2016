b92430ee70314666e7ac8037aa7a845fb1324dea
17changm95 <17changm95@millshighsch.org>
2016-08-26 08:43:24 -0700

    8/26/2016 Hello World Demonstration

---
+++ HelloWorld1/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Write a description of class Hello_world here.
+ * 
+ * @author Michael Chang 
+ * @version 1.0 8/26/2016
+ */
+
+public class HelloWorld {
+	public static void main(String[] args){
+		System.out.println("Hello World! From Michael Chang");
+		//Display the string
+	}
+}
