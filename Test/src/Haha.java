2b0b3a72e86d443ff0b333e8f8e3606c759918f9
17changm95 <17changm95@millshighsch.org>
2016-09-20 09:28:10 -0700

    Math Librarry Part 3

---
+++ Test/src/Haha.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Michael Chang
+ *
+ */
+public class Haha {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculate.average(8, 9)
+	}
+
+}
