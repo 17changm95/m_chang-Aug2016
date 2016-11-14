f40a49200649992ec1891488624814b8d62074ef
17changm95 <17changm95@millshighsch.org>
2016-11-14 00:10:04 -0800

    Stuff

---
+++ DrumKit/src/DrumKitTestDrive.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @author Michael Chang
+ * @version 1.0
+ * @since 11/8/2016
+ * 1°
+ *
+ */
+
+public class DrumKitTestDrive {
+	
+	public static void main(String[] args) {
+		DrumKit d = new DrumKit();
+		
+		d.snare = false;
+		d.playTopHat();
+		d.playSnare();
+		
+		if (d.snare == true) {
+			d.playSnare();
+		}
+	}
+	
+}
